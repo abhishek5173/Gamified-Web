9ae44cfa6262f3a16dd8fe254a40a8ba1ab7ff8b
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2026-05-13 18:07:40 +0530

    'Gamified Reality!'

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     }} className="h-screen w-full bg-neutral-900 flex items-center justify-center">
     
         <h1 className="text-4xl font-bold mb-4 text-center">
-          Welcome to the Next.js Starter Template!
+          Gamified Reality!
         </h1>
       
     </div>
